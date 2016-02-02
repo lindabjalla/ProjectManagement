210eb323afbaa0505244eec2158ce4bc1fb2d89d
lindabjalla <lindabjalla@gmail.com>
2016-02-02 08:11:53 +0100

    Tagits bort onödig import

--- ProjectManagement/src/main/java/se/grouprich/projectmanagement/service/ProjectManagementService.java
+++ ProjectManagement/src/main/java/se/grouprich/projectmanagement/service/ProjectManagementService.java
@@ -3,7 +3,6 @@ package se.grouprich.projectmanagement.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.UsesSunHttpServer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
