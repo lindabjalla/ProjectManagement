6438dddc3cdd27ef0386ce076115b82d167c8fb9
lindabjalla <lindabjalla@gmail.com>
2016-02-04 23:57:47 +0100

    implementerat equals och hashcode i WorkItem klass

--- ProjectManagement/src/main/java/se/grouprich/projectmanagement/model/WorkItem.java
+++ ProjectManagement/src/main/java/se/grouprich/projectmanagement/model/WorkItem.java
@@ -67,6 +67,35 @@ public class WorkItem extends AbstractEntity
 	}
 
 	@Override
+	public boolean equals(final Object other)
+	{
+		if (this == other)
+		{
+			return true;
+		}
+
+		if (other instanceof WorkItem)
+		{
+			WorkItem otherWorkItem = (WorkItem) other;
+			return title.equals(otherWorkItem.title) && user.equals(otherWorkItem.user) &&
+				   description.equals(otherWorkItem.description) && status.equals(otherWorkItem.status);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int result = 1;
+		result += title.hashCode() * 37;
+		result += user.hashCode() * 37;
+		result += description.hashCode() * 37;
+		result += status.hashCode() * 37;
+
+		return result;
+	}
+
+	@Override
 	public String toString()
 	{
 		return "WorkItem [id=" + id + ", title=" + title + ", user=" + user + ", description=" + description + ", status=" + status + "]";
