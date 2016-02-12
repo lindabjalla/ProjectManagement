d1df7b18a6c28866bec641430d945803cf96e5a0
lindabjalla <lindabjalla@gmail.com>
2016-02-12 19:06:21 +0100

    tog bort onödig query från TeamRepository

--- ProjectManagement/src/main/java/se/grouprich/projectmanagement/repository/TeamRepository.java
+++ ProjectManagement/src/main/java/se/grouprich/projectmanagement/repository/TeamRepository.java
@@ -9,9 +9,6 @@ import se.grouprich.projectmanagement.model.Team;
 
 public interface TeamRepository extends CrudRepository<Team, Long>
 {
-//	@Query("SELECT t FROM #{#entityName} t JOIN FETCH t.users")
-//	Iterable<Team> findAll();
-
 	@Query("SELECT t FROM #{#entityName} t order by t.controlNumber DESC")
 	Slice<Team> findAllOrderedByControlNumberDESC(Pageable pageable);
 }
